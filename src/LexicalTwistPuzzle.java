0e3d7f32ca158dafeb6ca71e345e8c27a4e4d1ea
Shivak007 <kshivaoffl2824@gmail.com>
2026-02-13 15:45:53 +0530

    [Shiva Kumar] Added: UC3: Validate Single Word Constraint.

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -11,5 +11,15 @@ public class LexicalTwistPuzzle {
 
         System.out.print("Enter second word: ");
         String secondWord = sc.nextLine();
+
+        if (firstWord.contains(" ")) {
+            System.out.println(firstWord + " is an invalid word");
+            return;
+        }
+
+        if (secondWord.contains(" ")) {
+            System.out.println(secondWord + " is an invalid word");
+            return;
+        }
     }
 }
